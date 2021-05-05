9ea5bca501e3db1cf8d910ec69ce1fa1eb6577f6
Eric Raymund Rex <58367989+ericraymundrex@users.noreply.github.com>
2021-05-05 13:52:21 +0530

    simple edit

--- src/Components/ExpensesItem.js
+++ src/Components/ExpensesItem.js
@@ -2,15 +2,17 @@ import "./ExpensesItem.css";
 import Date from './Date';
 import Card from './Card';
 function ExpensesItem(props){
+    let title=props.title;
     const clickHandle=()=>{
-        console.log("Hello");
+        title='Updated!!';
+        console.log(title);
     }
   
     return (
     <Card className="expense-item">
         <div ><Date date={props.date}/> </div>
         &nbsp;&nbsp;&nbsp;
-        <div className="expense-item__description">{props.title}</div>
+        <div className="expense-item__description">{title}</div>
         
         <div className="expense-item__price">$ {props.amount}</div>
         <button onClick={clickHandle}>Change state</button>
